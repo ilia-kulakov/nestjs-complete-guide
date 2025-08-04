ad3128cc91ceda88cf872fdd79fb2ae75712db28
Ilia Kulakov <Ilia_Kulakov@epam.com>
2025-08-04 14:52:03 +0200

    car-value: Throwing Exceptions

--- car-value/src/users/users.controller.ts
+++ car-value/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Query,
   Delete,
   Patch,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -22,8 +23,13 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(+id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
   }
 
   @Get()
