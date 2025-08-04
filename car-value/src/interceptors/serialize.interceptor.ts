53e9b6e31e6f7e501fc862c4e5f6659e8af45061
Ilia Kulakov <Ilia_Kulakov@epam.com>
2025-08-04 19:25:21 +0200

    car-value: Custom Data Serialization

---
+++ car-value/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { plainToInstance } from 'class-transformer';
+
+type ClassConstructor = new (...args: any[]) => {};
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private readonly dto: ClassConstructor) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data: ClassConstructor) => {
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
